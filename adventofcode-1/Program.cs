75cc4b30c5560a6066de53569166e0626470947a
xuri02 <t0bl4s.durst@gmail.com>
2020-12-01 19:53:21 +0100

    day 1

---
+++ adventofcode-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var numbers = (await File.ReadAllLinesAsync("numbers-1")).Select(int.Parse).ToArray();
+const int magic = 2020;
+foreach (var i in numbers)
+{
+    foreach (var j in numbers)
+    {
+        if (i + j == magic)
+            Console.WriteLine($"{i} * {j} = {i * j}");
+
+        foreach (var k in numbers)
+        {
+            if (i + j + k == magic)
+                Console.WriteLine($"{i} * {j} * {k} = {i * j * k}");
+        }
+    }
+}
