14b511a874e416d440169a34262682a5c5806bc0
xuri02 <t0bl4s.durst@gmail.com>
2020-12-02 15:10:22 +0100

    added ISolver interface

---
+++ adventofcode/ISolver.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace adventofcode
+{
+    public interface ISolver
+    {
+        ILogger<ISolver> logger { get; set; }
+        
+        string InFile { get; }
+
+        Task Init(string[] data);
+
+        Task Run();
+    }
+}
